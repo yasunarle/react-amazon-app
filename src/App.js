03ecbe3b92ed7db42e6db73da749a3627ac35698
Yasunari t <u7df25c588m737@gmail.com>
2020-07-19 23:59:16 +0900

    chore: created TheHeader.jsx file...

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 // CSS
-import "./App.css";
+import "./App.css"
+// Components
+import TheHeader from "./components/common/TheHeader"
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <TheHeader />
+        <Switch>
+          <Route path="/">
+            <h1>checkout</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/common/TheHeader.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function TheHeader() {
+  return (
+    <nav className="header">
+      <img
+        className="header__logo"
+        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="amazon logo"
+      />
+    </nav>
+  )
+}
+
+export default TheHeader
