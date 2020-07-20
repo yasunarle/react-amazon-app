fec8d5bb91853c9a680783e3911363ac0d32a671
Yasunari t <u7df25c588m737@gmail.com>
2020-07-20 16:13:09 +0900

    ✨feat: build Product component

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react"
-// Img
-// import amazonImg from "./amazon.jpg"
 // CSS
 import "./Home.css"
+// Components
+import Product from "components/common/AppProduct"
 
 function Home() {
   return (
@@ -12,6 +12,25 @@ function Home() {
         src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
         alt=""
       />
+      <div className="home__row">
+        <Product
+          title={"React Book"}
+          price={14}
+          rating={5}
+          image={
+            "https://prodimage.images-bn.com/pimages/9788850317714_p0_v1_s550x406.jpg"
+          }
+        />
+        <Product
+          title={"React Book"}
+          price={14}
+          rating={5}
+          image={
+            "https://prodimage.images-bn.com/pimages/9788850317714_p0_v1_s550x406.jpg"
+          }
+        />
+      </div>
+      {/* home__row */}
     </div>
   )
 }
